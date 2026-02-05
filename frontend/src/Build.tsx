68e0bb4bb2cbb5e4bbad4714d03119fb08cefcb6
Joshua Weston <bristol2801@gmail.com>
2026-02-05 17:31:50 +0000

    implemented getBuild func

--- frontend/src/Build.tsx
+++ frontend/src/Build.tsx
@@ -1,15 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { getBuild, type buildData } from "./api/getBuild";
+
+async function handleBuild(
+    id: string,
+    set: React.Dispatch<React.SetStateAction<buildData | undefined>>,
+) {
+    const build = await getBuild(id);
+    if (!build) {
+        return;
+    }
+    set(build);
+}
 
 function Build() {
     const navigate = useNavigate();
     const { id } = useParams();
 
+    const [build, setBuild] = useState<buildData | undefined>();
+
     useEffect(() => {
-        if (!id) navigate("/", { replace: true });
+        if (!id) {
+            navigate("/", { replace: true });
+            return;
+        }
+
+        const init = async () => {
+            await handleBuild(id, setBuild);
+        };
+        init();
     }, []);
 
-    return <div>build page</div>;
+    return <div>{build?.Id} page</div>;
 }
 
 export default Build;
--- frontend/src/api/getBuild.ts
+++ frontend/src/api/getBuild.ts
@@ -1,3 +1,23 @@
-export function getBuild(id: string) {
-    return id;
+import { API_BASE } from "./consts";
+
+export interface buildData {
+    LastModified: bigint;
+    Id: string;
+    Raw: string;
+    Data: JSON;
+}
+
+export async function getBuild(id: string) {
+    try {
+        const res = await fetch(`${API_BASE}/getBuild/${id}`, {
+            method: "GET",
+        });
+        if (!res.ok) throw new Error("Failed to get build");
+        const build: buildData = await res.json();
+        return build;
+    } catch (err) {
+        // Display error to user
+        console.log(err);
+        return null;
+    }
 }
