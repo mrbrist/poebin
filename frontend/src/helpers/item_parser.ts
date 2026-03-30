a9038f60da88b84d62f037c4b0e991e003ca41f5
Joshua Weston <bristol2801@gmail.com>
2026-03-30 17:00:33 +0100

    added simple item parsing

--- frontend/src/helpers/item_parser.ts
+++ frontend/src/helpers/item_parser.ts
@@ -1,5 +1,27 @@
+export interface itemData {
+    Name: string;
+    Base: string;
+    Rarity: string;
+    Quality: number;
+    Sockets: string;
+    ItemLevel: number;
+    Mods: [string];
+}
+
 export function parseItem(raw: any) {
     raw = String(raw);
     raw = raw.replace(/\t/g, "");
-    return raw.split("\n")[2];
+
+    let itemSplit = raw.split("\n");
+
+    let item: itemData = {
+        Name: itemSplit[2],
+        Base: itemSplit[3],
+        Rarity: itemSplit[1],
+        Quality: itemSplit[10],
+        Sockets: itemSplit[11],
+        ItemLevel: itemSplit[9],
+        Mods: itemSplit.slice(14, itemSplit.length).filter((n: any) => n),
+    };
+    return item;
 }
