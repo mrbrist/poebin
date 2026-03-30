699e07cf1ba1be2c03ffde322e67c83303439368
Joshua Weston <bristol2801@gmail.com>
2026-03-30 17:03:10 +0100

    item parsing need to account for some items not having the same attributes, for example jewels

--- frontend/src/build_segments/build_main.tsx
+++ frontend/src/build_segments/build_main.tsx
@@ -4,7 +4,7 @@ import { parseItem } from "../helpers/item_parser";
 function BuildMain({ build }: { build: buildData | undefined }) {
     return (
         <div className="col-span-9 rounded-2xl border border-slate-400 p-4">
-            {JSON.stringify(parseItem(build?.Data.Items.ItemList[15].Content))}
+            {JSON.stringify(parseItem(build?.Data.Items.ItemList[0].Content))}
         </div>
     );
 }
