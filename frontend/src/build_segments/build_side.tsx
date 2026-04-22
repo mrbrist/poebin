6ef26f5508c9e804366c13c8bcb9e0d76cbcd40b
Joshua Weston <bristol2801@gmail.com>
2026-04-22 14:08:53 +0100

    added copy to clipboard button

--- frontend/src/build_segments/build_side.tsx
+++ frontend/src/build_segments/build_side.tsx
@@ -2,6 +2,11 @@ import { type buildData } from "../api/getBuild";
 import { timeAgo } from "../helpers/timeAgo";
 
 function BuildSide({ build }: { build: buildData | undefined }) {
+    function copyBuildToClipboard() {
+        if (build?.Raw) {
+            navigator.clipboard.writeText(build.Raw);
+        }
+    }
     return (
         <aside className="col-span-3 space-y-6">
             <div className="rounded-2xl bg-slate-900 border border-slate-400 p-4 shadow-lg">
@@ -18,6 +23,32 @@ function BuildSide({ build }: { build: buildData | undefined }) {
                     <div>
                         <span className="text-zinc-200">Level: </span>
                         {build?.Data.Build.Level}
+                    </div>
+                    <div>
+                        <button
+                            className="inline-flex items-center gap-2 p-2 mt-4 mb-2 cursor-pointer bg-slate-700 hover:bg-slate-600 border border-slate-400 rounded-sm"
+                            onClick={copyBuildToClipboard}
+                        >
+                            <span id="default">
+                                <svg
+                                    className="w-4 h-4 text-fg-brand"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 7 2 2 4-4m-5-9v4h4V3h-4Z"
+                                    />
+                                </svg>
+                            </span>
+                            <span id="success" className=""></span>
+                            <span>Copy PoB Code</span>
+                        </button>
                     </div>
                     <div>{build ? timeAgo(build?.LastModified) : null}</div>
                 </div>
