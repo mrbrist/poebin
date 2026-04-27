0e36e2dc90f9ccca62157f4c064a15688db2c693
Joshua Weston <bristol2801@gmail.com>
2026-04-27 11:10:55 +0100

    start seperating items from item sets so i can display a dropdown for item sets

--- frontend/src/build_segments/items.tsx
+++ frontend/src/build_segments/items.tsx
@@ -2,18 +2,20 @@ import type { buildData } from "../api/getBuild";
 import { parseItem } from "../helpers/item_parser";
 
 function BuildItems({ build }: { build: buildData | undefined }) {
+    const itemList = build?.Data?.Items?.ItemList;
+    const sets = build?.Data?.Items?.ItemSets;
+
+    if (!itemList || !sets) return null;
+
+    const items = itemList.map((e: { Content: any }) => parseItem(e.Content));
+
     return (
         <div className="block text-left">
-            {build?.Data.Items.ItemList.map(
-                (item: { Content: any }, index: number) => (
-                    <span
-                        key={index}
-                        className={`block ${parseItem(item.Content).Rarity}`}
-                    >
-                        {parseItem(item.Content).Name}
-                    </span>
-                ),
-            )}
+            {items.map((item: any, index: any) => (
+                <span key={item.ID ?? index} className={`block ${item.Rarity}`}>
+                    {item.Name}
+                </span>
+            ))}
         </div>
     );
 }
