afe8191282814f463b69541cda893966e9347850
Joshua Weston <bristol2801@gmail.com>
2026-04-27 10:48:12 +0100

    Begin tree data extraction

--- frontend/src/build_segments/tree.tsx
+++ frontend/src/build_segments/tree.tsx
@@ -1,7 +1,13 @@
 import type { buildData } from "../api/getBuild";
 
 function BuildTree({ build }: { build: buildData | undefined }) {
-    return <div>Tree Goes Here</div>;
+    return (
+        <div>
+            {build?.Data.Tree.Specs[0].Title}
+            <br />
+            {build?.Data.Tree.Specs[0].Nodes}
+        </div>
+    );
 }
 
 export default BuildTree;
