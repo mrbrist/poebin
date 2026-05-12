466245d572072d09f3660ebd56080025023ac721
Joshua Weston <bristol2801@gmail.com>
2026-05-12 17:12:41 +0100

    Added tree selection dropdown

--- frontend/src/build_segments/tree.tsx
+++ frontend/src/build_segments/tree.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import type { buildData } from "../api/getBuild";
 
+// This might need rewriting because the tree selection dropdown is using the title and not a uuid of some sort ¯\_(ツ)_/¯
+
 function BuildTree({ build }: { build: buildData | undefined }) {
+    const trees = build?.Data?.Tree?.Specs;
+
+    const [selectedTreeTitle, setSelectedTreeTitle] = useState(trees[0].Title);
+    const selectedTree = trees.find(
+        (s: { Title: any }) => s.Title === selectedTreeTitle,
+    );
+
+    if (!selectedTree) return null;
+
     return (
-        <div>
-            {build?.Data.Tree.Specs[0].Title}
-            <br />
-            {build?.Data.Tree.Specs[0].Nodes}
+        <div className="block text-left">
+            {trees.length > 1 ? (
+                <select
+                    value={selectedTreeTitle}
+                    onChange={(e) => setSelectedTreeTitle(e.target.value)}
+                    className="mb-4 p-2 border rounded"
+                >
+                    {trees.map((set: any) => (
+                        <option key={set.Title} value={set.Title}>
+                            {set.Title}
+                        </option>
+                    ))}
+                </select>
+            ) : null}
+
+            {/* Items */}
+            <div>{selectedTree.Nodes}</div>
         </div>
     );
 }
