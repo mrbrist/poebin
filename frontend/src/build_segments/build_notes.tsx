311b7fd11291b2511b1cc41e978044af3b15c88b
Joshua Weston <bristol2801@gmail.com>
2026-05-12 14:13:25 +0100

    remove note section if there is not note for this build

--- frontend/src/build_segments/build_notes.tsx
+++ frontend/src/build_segments/build_notes.tsx
@@ -1,14 +1,18 @@
 import { type buildData } from "../api/getBuild";
 
 function BuildNotes({ build }: { build: buildData | undefined }) {
-    return (
-        <div className="col-span-9 rounded-2xl border border-slate-400 p-4">
-            <h2 className="text-lg font-semibold mb-4">Notes</h2>
-            <span className="whitespace-pre-line block text-left">
-                {build?.Data.Notes}
-            </span>
-        </div>
-    );
+    if (build?.Data.Notes.trim() == "") {
+        return <div></div>;
+    } else {
+        return (
+            <div className="col-span-9 rounded-2xl border border-slate-400 p-4">
+                <h2 className="text-lg font-semibold mb-4">Notes</h2>
+                <span className="whitespace-pre-line block text-left">
+                    {build?.Data.Notes}
+                </span>
+            </div>
+        );
+    }
 }
 
 export default BuildNotes;
